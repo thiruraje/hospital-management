0b504a8e8d594c2cacec02be64649b8e46e945dc
thiruraje <vklthiru2000@gmail.com>
2021-07-08 18:49:03 +0530

    add search option in accountant module and some corretions

--- backend/admin/routes/billRoute.js
+++ backend/admin/routes/billRoute.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 var express = require('express');
 
 const Patient = require('../../doctor/model/patientModel');
+const AdmitedPatient = require('../../doctor/model/admitedPatientModel');
+const DailyPatient = require('../../doctor/model/checkupDetailModel');
 
 
 
@@ -15,6 +17,68 @@ router.get('/patient-bill', async (req, res) => {
             patients : patients,
         }
         res.send(datas);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+});
+
+router.get('/admited-patient-bill', async (req, res) => {
+    try {
+        const admited_patients = await AdmitedPatient.aggregate([{
+                $lookup: {
+                    from: "patients", 
+                    localField: "patient",
+                    foreignField: "_id",
+                    as: "patient_info"
+                }
+            },
+            {   $unwind:"$patient_info" },
+            {
+                $lookup:{
+                    from: "rooms", 
+                    localField: "patient_info.room", 
+                    foreignField: "_id",
+                    as: "room_info"
+                }
+            },
+            {   $unwind:"$room_info" },
+            {
+                $lookup:{
+                    from: "doctors", 
+                    localField: "patient_info.doctor", 
+                    foreignField: "_id",
+                    as: "doctor_info"
+                }
+            },
+            {   $unwind:"$doctor_info" },
+        ]);
+
+        const daily_patients = await DailyPatient.aggregate([{
+                $lookup: {
+                    from: "patients", 
+                    localField: "patient",
+                    foreignField: "_id",
+                    as: "patient_info"
+                }
+            },
+            {   $unwind:"$patient_info" },
+            {
+                $lookup:{
+                    from: "doctors", 
+                    localField: "patient_info.doctor", 
+                    foreignField: "_id",
+                    as: "doctor_info"
+                }
+            },
+            {   $unwind:"$doctor_info" },
+        ]);
+
+        const data = {
+            daily_patients:daily_patients,
+            admited_patients:admited_patients
+            
+        }
+        res.send(data);
     } catch (error) {
         res.send({ message: error.message });
     }
